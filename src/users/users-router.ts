c2ce9607c175467d442a5e36b405fea650a9dc32
Julian Atkin <jatkindev@gmail.com>
2024-08-25 18:00:58 -0400

    Remove async/await

--- src/users/users-router.ts
+++ src/users/users-router.ts
@@ -3,32 +3,32 @@ import { create, destroy, findAll, findOne, update } from "./users-service.js";
 
 const usersRouter = express.Router();
 
-usersRouter.get("/", async (req, res, next) => {
+usersRouter.get("/", (req, res, next) => {
   const id = req.query.id as string | undefined;
-  if (id) return res.status(200).json(await findOne(parseInt(id)));
-  else return res.status(200).json(await findAll());
+  if (id) return res.status(200).json(findOne(parseInt(id)));
+  else return res.status(200).json(findAll());
 });
 
-usersRouter.post("/", async (req, res, next) => {
+usersRouter.post("/", (req, res, next) => {
   const data = req.body;
-  return res.status(200).json(await create(data));
+  return res.status(200).json(create(data));
 });
 
-usersRouter.patch("/", async (req, res, next) => {
+usersRouter.patch("/", (req, res, next) => {
   const id = req.query.id as string | undefined;
   const data = req.body;
 
   if (!id) return next();
 
-  return res.status(200).json(await update(parseInt(id), data));
+  return res.status(200).json(update(parseInt(id), data));
 });
 
-usersRouter.delete("/", async (req, res, next) => {
+usersRouter.delete("/", (req, res, next) => {
   const id = req.query.id as string | undefined;
 
   if (!id) return next();
 
-  return res.status(200).json(await destroy(parseInt(id)));
+  return res.status(200).json(destroy(parseInt(id)));
 });
 
 export default usersRouter;
